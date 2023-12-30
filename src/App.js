7e538a8bfbe37447ab18500d94d3f9412a8f3f7c
karim chebbi <karimchebbion@gmail.com>
2023-12-30 11:20:16 +0100

    first commi

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World !</h1>
+      <p>Hi my name is karim iw ish you the best.</p>
+      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/langfr-225px-Flag_of_Tunisia.svg.png' alt='tunisia flag'/>
+      <iframe width="665" height="480" src="https://www.youtube.com/embed/EWv2jnhZErc" title="HTML CSS JS projects (Beginner): 16 projects using HTML CSS and JavaScript" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
   );
 }
